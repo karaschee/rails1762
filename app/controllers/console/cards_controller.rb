2e4a1221eb8c930dbf56e472e99bf24871266443
karaschee <baishihaojc@gmail.com>
2014-02-12 18:21:18 +0800

    fix some bugs

--- app/controllers/console/cards_controller.rb
+++ app/controllers/console/cards_controller.rb
@@ -14,6 +14,7 @@ class Console::CardsController < Console::ConsoleBaseController
       format.js {
         render json: Card.all, methods: [:card_type_name]
       }
+      format.html {}
     end
   end
 
