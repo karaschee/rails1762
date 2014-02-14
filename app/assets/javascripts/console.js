1c756115c63da205da65ff9392743b3237987fc2
karaschee <baishihaojc@gmail.com>
2014-02-14 18:44:51 +0800

    add tinytip

--- app/assets/javascripts/console.js
+++ app/assets/javascripts/console.js
@@ -15,3 +15,4 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require seajs
+//= require seajs-style
--- vendor/assets/javascripts/seajs/index.js
+++ vendor/assets/javascripts/seajs/index.js
@@ -3,9 +3,10 @@
 seajs.config({
   base: '/sea-modules/',
   alias: {
-  '$': 'jquery/jquery/1.10.1/jquery',
-  'jquery': 'jquery/jquery/1.10.1/jquery',
-  'backbone': 'gallery/backbone/1.1.0/backbone',
-  'underscore': 'gallery/underscore/1.5.2/underscore'
+    '$': 'jquery/jquery/1.10.1/jquery',
+    'jquery': 'jquery/jquery/1.10.1/jquery',
+    'backbone': 'gallery/backbone/1.1.0/backbone',
+    'underscore': 'gallery/underscore/1.5.2/underscore',
+    'tinytip': 'karaschee/tinytip/1.0.0/tinytip'
   }
 });
