13ff8cdfb1621e63dad61631023eb6d1eb06d9e9
karaschee <baishihaojc@gmail.com>
2014-02-14 18:45:25 +0800

    add tinytip

---
+++ public/sea-modules/karaschee/tinytip/1.0.0/tinytip-debug.js
@@ -0,0 +1,48 @@
+define("karaschee/tinytip/1.0.0/tinytip-debug", [ "$-debug", "./tinytip-debug.css" ], function(require, exports, module) {
+    var $ = require("$-debug");
+    require("./tinytip-debug.css");
+    var defaults = {
+        txt: "操作进行中，请稍候",
+        container: "body",
+        css: {}
+    };
+    var Tinytip = function(options) {
+        options = $.extend({}, defaults, options);
+        // Initialize
+        var wrapper = $('<div style="display:none;" class="tinytip_wrapper"></div>');
+        var container = $(options.container);
+        container.append(wrapper.html(options.txt));
+        if (!container.is("body")) {
+            wrapper.css({
+                position: "absolute",
+                top: "30px"
+            });
+            container.css({
+                position: "relative"
+            });
+        }
+        wrapper.css(options.css);
+        this.wrapper = wrapper;
+        this.options = options;
+        Tinytip.tips = Tinytip.tips || [];
+        Tinytip.tips.push(this);
+    };
+    Tinytip.hide = function() {
+        $.each(Tinytip.tips, function(i, v) {
+            $(v).hide();
+        });
+    };
+    $.extend(Tinytip.prototype, {
+        show: function(txt) {
+            this.wrapper.html(txt);
+            this.wrapper.show();
+        },
+        hide: function() {
+            this.wrapper.hide();
+        },
+        reset: function() {
+            this.wrapper.html(this.options.txt);
+        }
+    });
+    module.exports = Tinytip;
+});
