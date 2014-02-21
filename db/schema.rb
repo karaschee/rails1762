59ca2f040442021c7f4c99d566d62f1140efafc3
karaschee <baishihaojc@gmail.com>
2014-02-21 17:36:26 +0800

    update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140219073401) do
+ActiveRecord::Schema.define(version: 20140221092601) do
 
   create_table "card_types", force: true do |t|
     t.string   "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20140219073401) do
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "thumbnail"
   end
 
   create_table "shows", force: true do |t|
@@ -34,6 +35,7 @@ ActiveRecord::Schema.define(version: 20140219073401) do
     t.string   "desc"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "thumbnail"
   end
 
   create_table "timelines", force: true do |t|
