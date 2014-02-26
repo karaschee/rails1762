81cd55caf89009728205dc4e4bfb42f7337a164e
karaschee <baishihaojc@gmail.com>
2014-02-26 12:48:53 +0800

    modify layout

--- vendor/assets/javascripts/underscore/index.js
+++ vendor/assets/javascripts/underscore/index.js
@@ -1,6 +1,6 @@
 //= require ./1.6.0/underscore-min
 
-// don't conflict with rails
+// Don't conflict with rails
 _.templateSettings = {
     interpolate: /\[\%\=(.+?)\%\]/g,
     evaluate: /\[\%(.+?)\%\]/g
