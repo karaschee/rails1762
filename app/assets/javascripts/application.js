d7ee71948c095e6aae12a7b68f9ff19c7a336d06
karaschee <baishihaojc@gmail.com>
2014-03-04 11:01:24 +0800

    update header css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,22 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require turbolinks
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
+
+// header scroll style change
+(function(){
+  var elemh = $('.g_header');
+  var scrollClass = 'g_header-scroll';
+  $(document).on('scroll', function(){
+    var t = $(document).scrollTop();
+    if(t > 5){
+      elemh.addClass(scrollClass);
+    }else {
+      elemh.removeClass(scrollClass);
+    }
+  });
+
+  $('#j_mnavBtn').on('click', function(){
+    $('#j_mnav').toggle();
+  });
+})();
